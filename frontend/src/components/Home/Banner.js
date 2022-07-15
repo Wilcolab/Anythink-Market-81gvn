d7b92dae27e382ae09c48d7a2e88e2ad6a127053
Gilad Foyer <giladfoyer@Gilads-MBP-2.lan>
2022-07-15 19:03:05 +0300

    Show search on click get

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../imgs/logo.png";
 import agent from "../../agent";
 
 const Banner = (props) => {
+  const [isSearchShown, setIsSearchShown] = useState(false);
+
   const handleChange = (ev) => {
     const searchTitleValue = ev.target.value.length > 2 ? ev.target.value : "";
     props.onSearchTitle(
@@ -16,15 +18,21 @@ const Banner = (props) => {
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
-          <span id="get-part">A place to get</span>
-          <input
-            id="search-box"
-            type="search"
-            placeholder="What is that you truly desire?"
-            results="0"
-            className="item-search"
-            onChange={handleChange}
-          />
+          <span>A place to</span>
+          <span id="get-part" onClick={() => setIsSearchShown(true)}>
+            {" "}
+            get{" "}
+          </span>
+          {isSearchShown && (
+            <input
+              id="search-box"
+              type="search"
+              placeholder="What is that you truly desire?"
+              results="0"
+              className="item-search"
+              onChange={handleChange}
+            />
+          )}
           <span> the cool stuff.</span>
         </div>
       </div>
